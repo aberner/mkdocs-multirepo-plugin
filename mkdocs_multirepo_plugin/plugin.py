e8bb89adb264c3e211015e2746ed2b90448fb1a0
Arne Berner <arne.berner@akerbp.com>
2026-03-17 11:50:02 +0100

    adding logging when deleting the temp_dir for all cases.

--- mkdocs_multirepo_plugin/plugin.py
+++ mkdocs_multirepo_plugin/plugin.py
@@ -359,4 +359,6 @@ class MultirepoPlugin(BasePlugin):
 
     def on_build_error(self, error):
         if self.temp_dir and self.config.get("cleanup"):
+            temp_dir = self.config.get("temp_dir")
+            log.info(f"Multirepo plugin is cleaning up {temp_dir}/")
             shutil.rmtree(str(self.temp_dir))
